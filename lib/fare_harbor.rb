81e0c8afb54a848687d3259ddf36063ac57b34db
Caleb <calebcowen@gmail.com>
2016-09-12 19:29:46 -0600

    Create items method for companies and write test. Closes#2

--- lib/fare_harbor.rb
+++ lib/fare_harbor.rb
@@ -1,17 +1,26 @@
-require "fareharbor_external_api/version"
-require "faraday"
+require 'fareharbor_external_api/version'
+require 'faraday'
 
 module FareHarbor
   module Affiliate
     class Companies
 
       def self.all
-        connection = Faraday.new("https://demo.fareharbor.com/api/external/v1/")
-        connection.params[:'api-app'] = ENV["API_APP_KEY"]
-        connection.params[:'api-user'] = ENV["API_USER_KEY"]
+        connection = Faraday.new('https://demo.fareharbor.com/api/external/v1/')
+        connection.params[:'api-app'] = ENV['API_APP_KEY']
+        connection.params[:'api-user'] = ENV['API_USER_KEY']
 
-        response = connection.get "companies"
-        data = JSON.parse(response.body)
+        response = connection.get 'companies/'
+        JSON.parse(response.body)
+      end
+
+      def self.items(company_name)
+        connection = Faraday.new('https://demo.fareharbor.com/api/external/v1/')
+        connection.params[:'api-app'] = ENV['API_APP_KEY']
+        connection.params[:'api-user'] = ENV['API_USER_KEY']
+
+        response = connection.get "companies/#{company_name}/items/"
+        JSON.parse(response.body)
       end
     end
   end
