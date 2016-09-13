5467b29eedfbedafe3f53d317321773bc371feda
Caleb <calebcowen@gmail.com>
2016-09-12 19:59:56 -0600

    Add availabilities method for companies and wrote test for it. Closes #3

--- lib/fare_harbor.rb
+++ lib/fare_harbor.rb
@@ -22,6 +22,15 @@ module FareHarbor
         response = connection.get "companies/#{company_name}/items/"
         JSON.parse(response.body)
       end
+
+      def self.availabilities(name, id, date)
+        connection = Faraday.new('https://demo.fareharbor.com/api/external/v1/')
+        connection.params[:'api-app'] = ENV['API_APP_KEY']
+        connection.params[:'api-user'] = ENV['API_USER_KEY']
+
+        response = connection.get "companies/#{name}/items/#{id}/minimal/availabilities/date/#{date}/"
+        JSON.parse(response.body)
+      end
     end
   end
 end
--- spec/companies_spec.rb
+++ spec/companies_spec.rb
@@ -32,4 +32,20 @@ describe FareHarbor::Affiliate::Companies do
       expect(item['location']).to eq('San Francisco Bay')
     end
   end
+
+  it 'retrieves all availabilities by date for specific item' do
+    VCR.use_cassette('companies#availabilities') do
+      availabilities_hash = FareHarbor::Affiliate::Companies.availabilities('sharktourshawaii', '1108', '2016-11-14')
+      availabilities = availabilities_hash['availabilities']
+      availability = availabilities.first
+
+      expect(availabilities.count).to eq 4
+      expect(availabilities_hash.class).to eq Hash
+      expect(availabilities.class).to eq Array
+      expect(availability['start_at']).to eq '2016-11-14T07:00:00-1000'
+      expect(availability['end_at']).to eq '2016-11-14T08:30:00-1000'
+      expect(availability['capacity']).to eq 18
+      expect(availability['pk']).to eq 435426
+    end
+  end
 end
