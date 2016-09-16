4adc87d2934a16810277e4eb9e18a5fa468b8635
Zack Forbing <zackforbing@gmail.com>
2016-09-15 19:31:25 -0600

    build availability, remove vcr from unit test

--- spec/fh/company/availability_spec.rb
+++ spec/fh/company/availability_spec.rb
@@ -6,16 +6,22 @@ describe FH::Company::Availability do
   end
 
   it 'responds to methods' do
-    VCR.use_cassette('company#availability') do
-      company = FH::Companies.find('bodyglove')
-      availability = company.availability(70050)
-      expect(availability).to respond_to :capacity
-      expect(availability).to respond_to :customer_type_rates
-      expect(availability).to respond_to :custom_field_instances
-      expect(availability).to respond_to :item
-      expect(availability).to respond_to :pk
-      expect(availability).to respond_to :start_at
-      expect(availability).to respond_to :end_at
-    end
+    availability = FH::Company::Availability.new(
+      capacity: 10,
+      customer_type_rates: [15, 10],
+      custom_field_instances: "",
+      item: { pk: 1867, name: "do the thing" },
+      pk: 4787,
+      start_at: "2015-01-22T11:30:00-1000",
+      end_at: "2015-01-22T13:30:00-1000"
+     )
+
+    expect(availability).to respond_to :capacity
+    expect(availability).to respond_to :customer_type_rates
+    expect(availability).to respond_to :custom_field_instances
+    expect(availability).to respond_to :item
+    expect(availability).to respond_to :pk
+    expect(availability).to respond_to :start_at
+    expect(availability).to respond_to :end_at
   end
 end
